533ee84583aa60d7940382b784e8c50c88c839a4
Kirill <>
2024-03-05 21:04:31 +0100

    Initial Upload

---
+++ src/PropertyList.jsx
@@ -0,0 +1,16 @@
+import Property from "./Property";
+import './PropertyList.css';
+
+function PropertyList({properties}) {
+    return (
+        <div className="PropertyList">
+            {properties.map(p => {
+                return <Property {...p} 
+                                key={p.id}            
+                />
+            })}
+        </div>
+    );
+}
+
+export default PropertyList;
